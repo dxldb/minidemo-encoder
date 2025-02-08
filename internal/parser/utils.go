f2d842653d3359423e7a22bcacc2e6f162f9f9ab
dxldb <114335970+dxldb@users.noreply.github.com>
2025-02-08 11:49:49 +0800

    Update utils.go

--- internal/parser/utils.go
+++ internal/parser/utils.go
@@ -3,8 +3,8 @@ package parser
 import (
 	"math"
 
-	encoder "github.com/hx-w/minidemo-encoder/internal/encoder"
-	ilog "github.com/hx-w/minidemo-encoder/internal/logger"
+	encoder "github.com/dxldb/minidemo-encoder/internal/encoder"
+	ilog "github.com/dxldb/minidemo-encoder/internal/logger"
 	common "github.com/markus-wa/demoinfocs-golang/v3/pkg/demoinfocs/common"
 )
 
